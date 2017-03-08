0dc025dad5e607eb46b73d559cc3f5fc6b3a1b0a
Morgan Fogarty <mofo37@gmail.com>
2017-03-07 17:45:52 -0800

    Tried to make a constructor function happen. Also added styling to home page. Still working on all of it.

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var addedProjects = [];
+
+var Projects = function(input) {
+  this.name = input.name;
+  this.homePage = input.homePage;
+  this.author = input.author;
+  this.projectLink = input.projectLink;
+  this.description = input.description;
+};
+
+Projects.prototype.toHtml = function() {
+  var newProject = $('article.template').clone();
+  newProject.find('name').text(this.name);
+  newProject.attr('project-image', this.homePage);
+  newProject.attr('author', this.author);
+  newProject.attr('project-link', this.projectLink);
+  newProject.find('description').toHtml(this.description);
+  return newProject;
+};
+newProject.removeClass();
