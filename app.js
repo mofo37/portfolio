ac400f7d1d5afbac1d36745b02b38a2e708a8497
Morgan Fogarty <mofo37@gmail.com>
2017-03-10 21:00:50 -0800

    got rid of some error messages, added in JQuery link. Duh.

--- app.js
+++ app.js
@@ -3,21 +3,33 @@
 var projects = [];
 
 function Project (input) {
-  this.name = input.name;
+  this.title = input.title;
   this.homePage = input.homePage;
   this.author = input.author;
   this.projectLink = input.projectLink;
   this.description = input.description;
 };
 
-Projects.prototype.toHtml = function() {
+Project.prototype.toHtml = function() {
   var $newProject = $('article.template').clone();
-  $newProject.find('name').text(this.name);
+  $newProject.find('h1:first').text(this.title);
   $newProject.find('project-image', this.homePage);
   $newProject.attr('author', this.author);
-  $newProject.attr('project-link', this.projectLink);
-  $newProject.find('description').toHtml(this.description);
+  $newProject.find('project-link a').attr('href', this.projectLink);
+  $newProject.find('description').text(this.description);
+  console.log('Hi');
   return $newProject;
 };
 
-$newProject.removeClass();
+sourceData.forEach(function(ele) {
+  projects.push(new Project(ele));
+});
+
+
+projects.forEach(function(Project){
+  $('#articles').append(Project.toHtml());
+});
+
+
+var x = new Project(projects);
+x.toHtml();
