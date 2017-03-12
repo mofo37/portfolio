56ad89067c39090f8c73b188f1ec8a248df8c26f
Morgan Fogarty <mofo37@gmail.com>
2017-03-11 18:10:02 -0800

    got info from source data to display. wooohooooo

--- app.js
+++ app.js
@@ -1,35 +1,27 @@
 'use strict';
 
-var projects = [];
-
 function Project (input) {
   this.title = input.title;
   this.homePage = input.homePage;
   this.author = input.author;
   this.projectLink = input.projectLink;
   this.description = input.description;
-};
+}
 
 Project.prototype.toHtml = function() {
-  var $newProject = $('article.template').clone();
+  var $newProject = $('article.template').clone().removeClass('template');
   $newProject.find('h1:first').text(this.title);
-  $newProject.find('project-image', this.homePage);
-  $newProject.attr('author', this.author);
-  $newProject.find('project-link a').attr('href', this.projectLink);
-  $newProject.find('description').text(this.description);
-  console.log('Hi');
+  $newProject.find('.project-image', this.homePage);
+  $newProject.find('.author').text(this.author);
+  $newProject.find('.project-link a').attr('href', this.projectLink);
+  $newProject.find('.description').html(this.description);
   return $newProject;
 };
 
+Project.prototype.render = function() {
+  $('main').append(this.toHtml());
+};
+
 sourceData.forEach(function(ele) {
-  projects.push(new Project(ele));
+  new Project(ele).render();
 });
-
-
-projects.forEach(function(Project){
-  $('#articles').append(Project.toHtml());
-});
-
-
-var x = new Project(projects);
-x.toHtml();
